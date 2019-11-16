3bc6cc9a51d15b5e3ea6c5b7d1ee0869a1964d98
Swegoverload456 <juanvelasquez983@gmail.com>
2019-11-16 03:35:45 -0500

    Added Randomization to Rotors
    Made an option at the beginning that allows you to choose if you want to randomize the rotors and their positions for fun.

--- Text Based Enigma Machine/src/main.java
+++ Text Based Enigma Machine/src/main.java
@@ -1,7 +1,9 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class main {
 
+    private static String r;
     private static String b;
     private static String inputM = "";
     private static int r1Num;
@@ -12,11 +14,27 @@ public class main {
     private static int r3Pos;
     private static enigmaMachine m;
     private static boolean encrypt;
+    private static boolean random;
+
 
     public static void main(String[] args){
 
         Scanner in = new Scanner(System.in);
 
+        //Asking if you want the rotors randomized
+        System.out.println("Type 'Y' if you would like to randomize the Rotors? If not type 'N' ");
+        r = in.nextLine();
+        if(r.equals("Y") || r.equals("y")){
+            random = true;
+            genRandomRotorNumbers();
+            System.out.println("Rotor 1 #: " + r1Num + " Rotor 1 Pos: " + r1Pos);
+            System.out.println("Rotor 2 #: " + r2Num + " Rotor 2 Pos: " + r2Pos);
+            System.out.println("Rotor 3 #: " + r3Num + " Rotor 3 Pos: " + r3Pos);
+        }
+        else if(r.equals("N") || r.equals("n")){
+            random = false;
+        }
+        //Asking if you are encrypting a word or decrypting a word
         System.out.println("Type 'E' if encrypting or 'D' if decrypting:");
         b = in.nextLine();
         if(b.equals("E") || b.equals("e")){
@@ -25,46 +43,81 @@ public class main {
         else if(b.equals("D") || b.equals("d")){
             encrypt = false;
         }
+        //Case for the previous action
         if(encrypt) {
 
-            System.out.println("What character would you like to encrypt?");
+            System.out.println("What text would you like to encrypt?");
             inputM = in.nextLine();
-            System.out.println("What is the number of the 1st rotor?");
-            r1Num = in.nextInt();
-            System.out.println("What is the position of the 1st rotor?");
-            r1Pos = in.nextInt();
-            System.out.println("What is the number of the 2nd rotor?");
-            r2Num = in.nextInt();
-            System.out.println("What is the position of the 2nd rotor?");
-            r2Pos = in.nextInt();
-            System.out.println("What is the number of the 3rd rotor?");
-            r3Num = in.nextInt();
-            System.out.println("What is the position of the 3rd rotor?");
-            r3Pos = in.nextInt();
-            m = new enigmaMachine(inputM, r1Num, r1Pos, r2Num, r2Pos, r3Num, r3Pos);
-            System.out.println("Encrypted Result: " + m.magic());
+            while(!random) {
+                System.out.println("What is the number of the 1st rotor?");
+                r1Num = in.nextInt();
+                System.out.println("What is the position of the 1st rotor?");
+                r1Pos = in.nextInt();
+                System.out.println("What is the number of the 2nd rotor?");
+                r2Num = in.nextInt();
+                System.out.println("What is the position of the 2nd rotor?");
+                r2Pos = in.nextInt();
+                System.out.println("What is the number of the 3rd rotor?");
+                r3Num = in.nextInt();
+                System.out.println("What is the position of the 3rd rotor?");
+                r3Pos = in.nextInt();
+                random = true;
+            }
+                m = new enigmaMachine(inputM, r1Num, r1Pos, r2Num, r2Pos, r3Num, r3Pos);
+                System.out.println("Encrypted Result: " + m.magic());
         }
         else if(!encrypt){
 
-            System.out.println("What character would you like to decrypt?");
+            System.out.println("What text would you like to decrypt?");
             inputM = in.nextLine();
-            System.out.println("What is the number of the 1st rotor?");
-            r1Num = in.nextInt();
-            System.out.println("What is the position of the 1st rotor?");
-            r1Pos = in.nextInt();
-            System.out.println("What is the number of the 2nd rotor?");
-            r2Num = in.nextInt();
-            System.out.println("What is the position of the 2nd rotor?");
-            r2Pos = in.nextInt();
-            System.out.println("What is the number of the 3rd rotor?");
-            r3Num = in.nextInt();
-            System.out.println("What is the position of the 3rd rotor?");
-            r3Pos = in.nextInt();
-            m = new enigmaMachine(inputM, r1Num, r1Pos, r2Num, r2Pos, r3Num, r3Pos);
-            System.out.println("Encrypted Result: " + m.magic());
-
+            while(!random) {
+                System.out.println("What is the number of the 1st rotor?");
+                r1Num = in.nextInt();
+                System.out.println("What is the position of the 1st rotor?");
+                r1Pos = in.nextInt();
+                System.out.println("What is the number of the 2nd rotor?");
+                r2Num = in.nextInt();
+                System.out.println("What is the position of the 2nd rotor?");
+                r2Pos = in.nextInt();
+                System.out.println("What is the number of the 3rd rotor?");
+                r3Num = in.nextInt();
+                System.out.println("What is the position of the 3rd rotor?");
+                r3Pos = in.nextInt();
+                random = true;
+            }
+                m = new enigmaMachine(inputM, r1Num, r1Pos, r2Num, r2Pos, r3Num, r3Pos);
+                System.out.println("Encrypted Result: " + m.magic());
         }
 
     }
 
+    public static void genRandomRotorNumbers(){
+
+        Random rand = new Random();
+
+        r1Pos = rand.nextInt(26);
+        r2Pos = rand.nextInt(26);
+        r2Pos = rand.nextInt(26);
+
+        r1Num = rand.nextInt(5);
+        r2Num = r1Num;
+        r3Num = r1Num;
+        while(r2Num == r1Num){
+
+            r2Num = rand.nextInt(5);
+
+        }
+        while(r3Num == r2Num || r3Num == r1Num){
+
+            r3Num = rand.nextInt(5);
+
+        }
+
+        r1Num++;
+        r2Num++;
+        r3Num++;
+
+    }
+
 }
+
